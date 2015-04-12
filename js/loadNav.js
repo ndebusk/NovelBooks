d5c746c0d1a0621f8a1926d9f6137b87b03f934d
ndebusk <ngdebusk@gmail.com>
2015-04-12 13:22:49 -0500

    Fixed error with regular user login showing navbar. Made consistency changes across html

--- js/loadNav.js
+++ js/loadNav.js
@@ -36,8 +36,13 @@ function validate() {
             // change to home screen on successful login attempt
             // window.location = 'index.html';
         } else if (response == 1) { //The needed cookie wasn't found
-            alert("It didn't work!");
+            //alert("It didn't work!");
             //Do nothing
+            document.getElementById("navbar").innerHTML = "<ul class='nav navbar-nav'>" + 
+                "<li><a href='account.html'><i class='fa fa-user'></i> Account</a></li>" + 
+                "<li><a href='checkout.html'><i class='fa fa-crosshairs'></i> Checkout</a></li>" + 
+                "<li><a href='cart.html'><i class='fa fa-shopping-cart'></i> Cart</a></li>" + 
+                "<li><a href='#' class='active'><i class='fa fa-lock'></i> Logout</a></li></ul>";
         }
     });
 }
