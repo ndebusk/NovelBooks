ce16255b17eb1f714dc6ceda1c9fdf8858b3cb96
Sean <sean_dyer@ymail.com>
2015-04-12 12:15:55 -0500

    Redirect working again in login
    Changed one line, that’s all set. Thanks!

--- js/loginValidate.js
+++ js/loginValidate.js
@@ -34,7 +34,7 @@ function validate() {
             document.getElementById("navbar").innerHTML = "<ul class='nav navbar-nav'><li><a href='itementry.html'><i class='fa fa-shield'></i> Admin</a></li><li><a href='account.html'><i class='fa fa-user'></i> Account</a></li><li><a href='checkout.html'><i class='fa fa-crosshairs'></i> Checkout</a></li><li><a href='cart.html'><i class='fa fa-shopping-cart'></i> Cart</a></li><li><a href='#' class='active'><i class='fa fa-lock'></i> Logout</a></li></ul>";
             
             // change to home screen on successful login attempt
-            // window.location = 'index.html';
+            window.location.href = 'index.html';
         } else if (response == 1) { // give invalid login error message
             document.getElementById("error").innerHTML = "Your username or password are invalid. Login Failed.";
         }
