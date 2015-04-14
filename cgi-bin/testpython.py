a5bfe236376b2124677a575ce375c5997f891abb
Sean <sean_dyer@ymail.com>
2015-04-14 16:39:48 -0500

    Adding file to test your Python with
    Here you go!

---
+++ cgi-bin/testpython.py
@@ -0,0 +1,14 @@
+#!/Applications/MAMP/Library/bin/python2.7
+import mysql.connector
+import cgi
+import os
+import Cookie
+import urllib
+import urllib2
+
+
+
+print ("Content-Type: text/html\n\n")
+
+
+print "<p>Test</p>"
