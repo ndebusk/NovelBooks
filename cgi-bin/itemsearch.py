f565dbbbd4e278d9f4be2c875c0b645ae9037ad6
ndebusk <ngdebusk@gmail.com>
2015-04-18 18:34:43 -0500

    Found and solved problem in item search queries, was displaying multiple entries of the book if it had several authors

--- cgi-bin/itemsearch.py
+++ cgi-bin/itemsearch.py
@@ -23,6 +23,8 @@ config = {
 #Creates the connection and cursor.
 cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor()
+cnx2 = mysql.connector.connect(**config)
+cursor2 = cnx2.cursor()
 
 #I build the query string in two lines because it's such a long string.
 #if (str(type) == "author"):
@@ -32,25 +34,33 @@ cursor = cnx.cursor()
 #else:
 
 if searchtype == "all":
-    queryStringBook = "SELECT title, author, image, book.isbn FROM book, author WHERE book.isbn = author.isbn"
+    queryStringBook = "SELECT isbn, title, image FROM book"
     cursor.execute(queryStringBook)
+    
 elif searchtype == "genre":
-    queryStringBook = "SELECT title, author, image, book.isbn FROM book, author, genre WHERE book.isbn = author.isbn AND book.isbn = genre.isbn AND genre.genre = '" + searchterm + "'"
+    queryStringBook = "SELECT book.isbn, title, image FROM book, genre WHERE book.isbn = genre.isbn AND genre.genre = '" + searchterm + "'"
     cursor.execute(queryStringBook)
+    
 elif searchtype == "formatType":
-    queryStringBook = "SELECT title, author, image, book.isbn FROM book, author, format WHERE book.isbn = author.isbn AND book.isbn = format.isbn AND format.format = '" + searchterm + "'"
+    queryStringBook = "SELECT book.isbn, title, image FROM book, format WHERE book.isbn = format.isbn AND format.format = '" + searchterm + "'"
     cursor.execute(queryStringBook)
+    
 else:
-    queryStringBook = "SELECT title, author, image, book.isbn FROM book, author WHERE(" + searchtype + " LIKE '%" + str(searchterm) + "%') AND (book.isbn = author.isbn)"
+    queryStringBook = "SELECT book.isbn, title, image FROM book WHERE (" + searchtype + " LIKE '%" + str(searchterm) + "%')"
     cursor.execute(queryStringBook)
 
 print '<h2 class="title text-center">%s</h2>' % searchterm
 for row in cursor:
     print '<div class="col-sm-4"><div class="product-image-wrapper"><div class="single-products"><div class="productinfo text-center">'
     print '<img src="%s" alt="" />' % row[2]
-    print '<h2>%s</h2>' % row[0]
-    print '<p>%s</p>' %  row[1]
-    print '<form action="product-details.html" method="get"><input id="isbn" type="hidden" name="isbn" value="%s"/>' % row[3]
+    print '<h2>%s</h2>' % row[1]
+    queryStringAuthor = "SELECT author FROM author WHERE isbn = '" + row[0] + "'"
+    cursor2.execute(queryStringAuthor)
+    for item in cursor2:
+        print '<p>%s</p>' %  item[0]
+    print '<form action="product-details.html" method="get"><input id="isbn" type="hidden" name="isbn" value="%s"/>' % row[0]
     print '<button id="search" type="submit" class="btn btn-default add-to-cart"><i class="fa fa-bars"></i>Details</button></form></div></div></div></div>'
+    cnx2.commit()
 cnx.commit()
-cnx.close();
+cnx.close()
+cnx2.close();
